9822e565786315b4703184713f8498d098e679e8
Mark-david <ojukwudavid59@outlook.com>
2023-05-02 20:09:59 +0100

    apav_originals

--- app.js
+++ app.js
@@ -8,29 +8,29 @@ btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
 
-const allLinks = document.querySelectorAll('a:link');
-console.log(allLinks);
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
+// const allLinks = document.querySelectorAll('a:link');
+// console.log(allLinks);
+// allLinks.forEach(function (link) {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute('href');
 
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
+//     if (href === '#')
+//       window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth',
+//       });
 
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href);
-      console.log(sectionEl);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
-    }
+//     if (href !== '#' && href.startsWith('#')) {
+//       const sectionEl = document.querySelector(href);
+//       console.log(sectionEl);
+//       sectionEl.scrollIntoView({ behavior: 'smooth' });
+//     }
 
-    if (link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
-  });
-});
+//     if (link.classList.contains('main-nav-link'))
+//       headerEl.classList.toggle('nav-open');
+//   });
+// });
 
 const sectionHeroEl = document.querySelector('.carousel');
 
